82bfc7f52ad0d1ef0e8b597ce8114fdc4c903665
Sam Leal <Leal.Samir@Outlook.com>
2023-07-28 19:48:58 -0400

    Adding a solution for CollatzConjecture.cs, and moving other Solutions into separate folders

---
+++ samples/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Calculator
+{
+
+    public static void Main(String[] args)
+    {
+
+        // get input from the user 
+        Console.Write("Please enter a value for x: ");
+        int x = int.Parse(Console.ReadLine());
+
+        // get input from the user 
+        Console.Write("Please enter a value for y: ");
+        int y = int.Parse(Console.ReadLine());
+
+        // print output of x + y 
+        Console.WriteLine(Add(x, y));
+
+        // prevent console window from closing 
+        Console.ReadLine(); 
+
+    }
+
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+
+}
