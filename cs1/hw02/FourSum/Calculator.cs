e3423fdab269c7ad9e22dbea91447b09162748e9
Sam Leal <Leal.Samir@Outlook.com>
2023-07-29 07:26:36 -0400

    Adding a solution for HW02 - FourSum.cs

---
+++ cs1/hw02/FourSum/Calculator.cs
@@ -0,0 +1,38 @@
+﻿/************************************************************************
+ * Name: Sam Leal 
+ * Date: 07/29/2023 
+ * Filename: Calculator.cs 
+ * Description: Performs mathematical operations on numbers. 
+ ************************************************************************/
+public class Calculator
+{
+        
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+
+    // gets the sum of all the elements in an array 
+    public int Sum(int[] nums)
+    {
+
+        // declare local variables 
+        int sum = 0;
+
+        // gets the sum of the array 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+
+        // returns the answer 
+        return sum;
+
+    }
+
+}
