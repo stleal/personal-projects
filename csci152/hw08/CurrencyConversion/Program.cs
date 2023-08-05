ae691569b6df0ec0cb61531fd80cf9e12a33eb88
Sam Leal <Leal.Samir@Outlook.com>
2023-08-05 18:37:01 -0400

    Adding solutions for some additional Homework assignments from hw08 - hw10 in CSCI152

---
+++ csci152/hw08/CurrencyConversion/Program.cs
@@ -0,0 +1,72 @@
+﻿/*********************************************************************************************
+ * Name: Sam Leal 
+ * Date: 08/05/2023 
+ * Filename: CurrencyConversion.cs 
+ * Description: Write a program that first asks the user to type today's price for one dollar in 
+ * Brazilian reals, then reads U.S. dollar values and converts each to real. Use 0 as sentinel. 
+********************************************************************************************/
+public class Program
+{
+
+    // main 
+    public static void Main(string[] args)
+    {
+
+        // declare local variables 
+        string input;
+        double exchangeRate; 
+
+        // initialize variables 
+        input = string.Empty;
+
+        // prompts the user to enter today's price for one dollar in BRL 
+        Console.Write("Please enter today's exchange rate for one dollar in Brazilian reals: "); 
+        exchangeRate = Convert.ToDouble(Console.ReadLine());
+
+        // infinite loop  
+        while (input != "exit" && input != "stop")
+        {
+
+            // get input from the user 
+            Console.Write("Please enter a U.S. dollar value: ");
+            input = Console.ReadLine();
+
+            // stop the application 
+            if (input == "exit" || input == "stop")
+                break;
+
+            try
+            {
+
+                // converts a U.S. dollar value into BRL 
+                ConvertFromUSDToBRL(Convert.ToDouble(input), exchangeRate);
+
+            }
+            catch(Exception ex)
+            {
+                
+                // displays the error message in the console 
+                Console.WriteLine(ex.ToString()); 
+
+            }
+                
+        }
+
+    }
+
+    // converts a U.S. dollar value into BRL 
+    public static void ConvertFromUSDToBRL(double usdValue, double exchangeRate)
+    {
+
+        // declare local variables 
+        double answer;
+
+        // converts the USD value into BRL 
+        answer = usdValue * exchangeRate;
+
+        // returns the answer 
+        Console.WriteLine(usdValue + " U.S. dollars = " + answer + " Brazilian reals (BRL)"); 
+
+    }
+
+} 
---
+++ csci152/hw08/OysterBar/Program.cs
@@ -0,0 +1,44 @@
+﻿/*********************************************************************************************
+ * Name: Sam Leal 
+ * Date: 08/05/2023 
+ * Filename: OysterBar.cs 
+ * Description: You need to control the number of people who can be in an oyster bar at the same time. 
+ * Groups of people can always leave the bar, but a group cannot enter the bar if they would make the 
+ * number of people in the bar exceed the maximum of 100 occupants. Write a program that reads the sizes
+ * of the groups that arrive or depart. Use negative numbers for depatures. After each input, display the 
+ * current number of occupants. As soon as the bar holds the maximum number of people, report that the bar 
+ * is full and exit the program. 
+********************************************************************************************/
+public class Program
+{
+
+    // main 
+    public static void Main(string[] args)
+    {
+
+        // declare local variables 
+        int numberOfOccupants;
+
+        // initialize local variables 
+        numberOfOccupants = 0;
+
+        // infinite loop  
+        while (numberOfOccupants < 100)
+        {
+
+            // get input from the user 
+            Console.Write("Please enter the size of a group that is arriving or departing (use negative numbers for departure): ");
+            numberOfOccupants += int.Parse(Console.ReadLine());
+            
+            // displays the current number of occupants
+            Console.WriteLine("Number of occupants: " + numberOfOccupants);
+
+            // stop the application 
+            if (numberOfOccupants >= 100)
+                break;
+
+        }
+
+    }
+
+}
