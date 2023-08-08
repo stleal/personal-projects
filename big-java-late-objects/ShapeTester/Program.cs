98208b67a70c8b90dc138f89560d1b66cc0b5b8a
Sam Leal <Leal.Samir@Outlook.com>
2023-08-07 20:47:43 -0400

    Adding solutions for HW assignments in CSCI252, plus an additional solution from the Book, ShapeTester.cs

---
+++ big-java-late-objects/ShapeTester/Program.cs
@@ -0,0 +1,72 @@
+﻿/**************************************************************************************************************
+ * Name: Sam Leal 
+ * Date: 08/07/2023 
+ * Filename: ShapeTester.cs 
+ * Description: Write a program that computes the volume and surface area of a sphere with radius r, 
+ * a cylinder with a circular base with radius r and height h, and a cone with a circular base with radius r 
+ * and height h. Then write a program that prompts the user for the values of r and h, calls the six methods, 
+ * and prints the results. 
+ * 
+ * You should implement the following methods: 
+ *      public static double sphereVolume(double r) 
+ *      public static double sphereSurface(double r) 
+ *      public static double cylinderVolume(double r, double h) 
+ *      public static double coneVolume(double r, double h) 
+ *      public static double coneSurface(double r, double h) 
+ *  
+ *  For fun, we are going to implement these using the following classes: 
+ *      Shape.cs, Sphere.cs, Cylinder.cs, and Cone.cs 
+ *  
+ *  Remember to make your Sphere.cs, Cylinder.cs, and Cone.cs classes extend the Shape.cs class. 
+**************************************************************************************************************/
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        // declare local variables 
+        double r, h, mass; 
+
+        // initialize local variables 
+        r = 0.94;
+        h = 1.03;
+        mass = ConvertLbsToKgs(120); 
+
+        // sphere 
+        Shape sphere = new Shape(r, null);
+        Console.WriteLine("Volume of a Sphere with a radius of " + r + " = " + sphere.SphereVolume()); 
+        Console.WriteLine("Surface area of a Sphere with a radius of " + r + " = " + sphere.SphereSurface());
+
+        // cylinder 
+        Shape cylinder = new Shape(r, h);
+        Console.WriteLine("Volume of a Cylinder with a radius of " + r + " and height of " + h + " = " + cylinder.CylinderVolume());
+        Console.WriteLine("Surface area a Cylinder with a radius of " + r + " and height of " + h + " = " + cylinder.CylinderSurface());
+
+        // cone 
+        Shape cone = new Shape(r, h);
+        Console.WriteLine("Volume of a Cone with a radius of " + r + " and height of " + h + " = " + cone.ConeVolume());
+        Console.WriteLine("Surface area a Cone with a radius of " + r + " and height of " + h + " = " + cone.ConeSurface());
+
+        // additional physics exercises 
+        // density of a sphere 
+        sphere = new Shape(r, null, mass);
+        Console.WriteLine("Density of a Sphere with a mass of " + mass + "kgs = " + sphere.SphereDensity());
+
+        // density of a cylinder 
+        cylinder = new Shape(r, h, mass);
+        Console.WriteLine("Density of a Cylinder with a mass of " + mass + "kgs = " + cylinder.CylinderDensity());
+
+        // density of a cone 
+        cone = new Shape(r, h, mass);
+        Console.WriteLine("Density of a Cone with a mass of " + mass + "kgs = " + cone.ConeDensity());
+
+    }
+
+    // converts lbs to kgs 
+    public static double ConvertLbsToKgs(double lbs)
+    {
+        return (lbs * 0.453592); 
+    }
+
+}
---
+++ csci252/assignment1/PayCheckTester/Program.cs
@@ -0,0 +1,105 @@
+﻿/*******************************************************************************************************************************
+ * Name: Sam Leal 
+ * Date: 08/07/2023 
+ * Filename: PayCheckTester.cs 
+ * Description: In this program assignment, you will construct a class PayCheck. Your program will be used to print paychecks 
+ * for a group of student assistants. 
+ * 
+ * Your class should contain at least the following: 
+ * 
+ *      a) Three variables to represent the name, the hourly wage and hours worked. 
+ *      b) A method to deduct the federal and social security taxes. ( find about social security tax on the Internet.) 
+ *         For simplicity, you may assume that both the federal and social security taxes use fixed rates. 
+ *      c) A method to print a paycheck in certain format. 
+ *      d) Static variables contain information like school name, tax rate, and/or other object-independent values. 
+ *      
+ * You should supply a main method (in the same OR different class) to test your program.  
+ * 
+ * The name, hourly wages and hours worked should be input to the program (standard input or command line argument), 
+ * and the tax rates might be built-in values. 
+********************************************************************************************************************************/
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        // declare local variables 
+        PayCheck p; 
+        string input; 
+        bool running; 
+        int hoursWorked;
+        double hourlyWages;
+        string name, schoolName;
+
+        // initialize local variables 
+        running = true;
+
+        // display a welcome message 
+        Console.WriteLine("PayCheck Tester for Student Assistants\n");
+
+        // infinite loop 
+        while (running)
+        {
+
+            // prompt the user for the student assistant's name 
+            Console.Write("Please enter the student assistant's name: ");
+
+            // get input from the user 
+            input = Console.ReadLine();
+
+            // check input 
+            if (input == "exit" || input == "stop" || input == "")
+                break;
+
+            // save the name 
+            name = input;
+
+            // prompt the user for the student assistant's school name  
+            Console.Write("Please enter the student assistant's school name: ");
+
+            // get input from the user 
+            input = Console.ReadLine();
+
+            // check input 
+            if (input == "exit" || input == "stop" || input == "")
+                break;
+
+            // save the name 
+            schoolName = input;
+
+            // prompt the user for the student assistant's hourly wages 
+            Console.Write("Please enter the student assistant's hourly wages: ");
+
+            // get input from the user 
+            input = Console.ReadLine();
+
+            // check input 
+            if (input == "exit" || input == "stop" || input == "")
+                break;
+
+            // save the hourly wages 
+            hourlyWages = Double.Parse(input);
+
+            // prompt the user for the student assistant's hours worked  
+            Console.Write("Please enter the student assistant's hours worked: ");
+
+            // get input from the user 
+            input = Console.ReadLine();
+
+            // check input 
+            if (input == "exit" || input == "stop" || input == "")
+                break;
+
+            // save the hours worked 
+            hoursWorked = int.Parse(input);
+
+            // print the paycheck 
+            p = new PayCheck(name, schoolName, hourlyWages, hoursWorked); 
+            p.PrintPayCheck();
+
+        }
+
+    }
+
+}
