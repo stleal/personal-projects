b7c4897187b356279b153463e772e707ef6d384a
Sam Leal <Leal.Samir@Outlook.com>
2023-09-09 12:39:44 -0400

    Adding a solution for EncodeTheMessage.cs, DefuseTheBomb.cs, and CountPairs.cs

---
+++ online-exercises/leetcode/easy/CountPairs/CountPairs/Program.cs
@@ -0,0 +1,151 @@
+﻿/********************************************************************************************
+ * Name: Sam Leal 
+ * Date: 09/04/2023 
+ * Filename: CountPairs.cs 
+ * Description: Given a 0-indexed integer array nums of length n and an integer target, 
+ * return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+ * 
+ * Example 1: 
+ * Input: nums = [-1,1,2,3,1], target = 2 
+ * Output: 3 
+ * Explanation: There are 3 pairs of indices that satisfy the conditions in the statement: 
+ * - (0, 1) since 0 < 1 and nums[0] + nums[1] = 0 < target 
+ * - (0, 2) since 0 < 2 and nums[0] + nums[2] = 1 < target  
+ * - (0, 4) since 0 < 4 and nums[0] + nums[4] = 0 < target 
+ * Note that (0, 3) is not counted since nums[0] + nums[3] is not strictly less than the target.
+ * 
+ * For the purpose of this exercise, I've provided a CountPairs() method, which randomly generates 
+ * an array of data, given the size of the array and range of the minimum and maximum value.  
+ * 
+ *  CountPairs(min, max, size, target, "output.txt"); 
+ * 
+ * If target is null, then it will also randomly generate a target value between 0, and max value. 
+ * 
+ *  CountPairs(min, max, size, null, "output.txt"); 
+ * 
+********************************************************************************************/
+public class Program
+{
+
+    // main 
+    public static void Main(string[] args)
+    {
+
+        // unit test case 01 
+        CountPairs(-256, 256, 128, 87, "Test.txt");
+
+        // unit test case 02 
+        CountPairs(-256, 256, 128, 94, "Text02.txt");
+
+        // unit test case 02 
+        CountPairs(-256, 256, 128, 103, "Text03.txt");
+
+    }
+
+    // counts the number of pairs in the array 
+    public static void CountPairs(int minValue, int maxValue, int size, int? target, string filename)
+    {
+
+        // declare local variables 
+        Random r;
+        int sum, count;
+        int[] nums, pair;
+        List<int[]> pairs;
+        bool result; 
+
+        // initialize local variables 
+        count = 0;
+        r = new Random();
+        nums = new int[size];
+        target = (target == null) ? r.Next(0, maxValue+1) : target;
+        pairs = new List<int[]>();
+        result = false;
+
+        // generate some random numbers between minValue, and maxValue 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = r.Next(minValue, maxValue+1);
+        }
+
+        // loop through the nums array 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i; j < nums.Length; j++)
+            {
+                if (i != j)
+                {
+                    sum = nums[i] + nums[j];
+                    if (sum < target)
+                    {
+                        count++;
+                        pair = new int[2];
+                        pair[0] = i;
+                        pair[1] = j;
+                        pairs.Add(pair);
+                    }
+                }
+            }
+        }
+
+        try
+        {
+
+            // write to file 
+            using (StreamWriter sw = new StreamWriter("../../../Output/" + filename))
+            {
+
+                // writes a message 
+                sw.WriteLine("Randomly generated array: ");
+
+                // writes the randomly generated array into the file 
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    sw.WriteLine("nums[" + i + "] = " + nums[i]);
+                }
+
+                // writes a blank line 
+                sw.WriteLine();
+
+                // writes a message 
+                sw.WriteLine("Pairs array: ");
+
+                // writes the pairs array into the file 
+                for (int i = 0; i < pairs.Count; i++)
+                {
+                    sw.WriteLine("(" + pairs[i][0] + ", " + pairs[i][1] + ") => " + (nums[pairs[i][0]] + nums[pairs[i][1]]));
+                }
+
+                // writes a blank line 
+                sw.WriteLine();
+
+                // writes the target 
+                sw.WriteLine("Target: " + target);
+
+                // writes the total number of pairs found 
+                sw.WriteLine("Total number of pairs: " + count);
+
+                // set result = true 
+                result = true; 
+
+            }
+
+        } 
+        catch (IOException ex)
+        {
+            Console.WriteLine("We're sorry, but there was an error writing or saving to the file: ");
+            Console.WriteLine(ex.Message);
+        }
+
+        // display a message 
+        if (result)
+        {
+            Console.WriteLine("Successfully wrote Pairs to Output file: " + filename); 
+        }
+        else
+        {
+            Console.WriteLine("Failed to write Pairs to Output file: " + filename); 
+        }
+
+    }
+
+}
---
+++ online-exercises/leetcode/easy/DefuseTheBomb/DefuseTheBomb/Program.cs
@@ -0,0 +1,245 @@
+﻿/********************************************************************************************************************************
+ * Name: Sam Leal 
+ * Date: 09/04/2023 
+ * Filename: DefuseTheBomb.cs 
+ * Description: You have a bomb to defuse, and your time is running out! Your informer will provide you with a circular array 
+ * code of length of n and a key k. 
+ * 
+ * To decrypt the code, you must replace every number. All the numbers are replaced simultaneously. 
+ * 
+ *   If k > 0, replace the ith number with the sum of the next k numbers. 
+ *   If k < 0, replace the ith number with the sum of the previous k numbers. 
+ *   If k == 0, replace the ith number with 0. 
+ *   
+ * As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1]. 
+ * Given the circular array code and an integer key k, return the decrypted code to defuse the bomb! 
+ * 
+ * Example 1: 
+ * Input: code = [5,7,1,4], k = 3 
+ * Output: [12,10,16,13] 
+ * Explanation: Each number is replaced by the sum of the next 3 numbers. The decrypted code is [7+1+4, 1+4+5, 4+5+7, 5+7+1]. 
+ * Notice that the numbers wrap around.
+ * 
+ * Example 2: 
+ * Input: code = [1,2,3,4], k = 0 
+ * Output: [0,0,0,0] 
+ * Explanation: When k is zero, the numbers are replaced by 0.  
+ * 
+ * Example 3: 
+ * Input: code = [2,4,9,3], k = -2 
+ * Output: [12,5,6,13] 
+ * Explanation: The decrypted code is [3+9, 2+3, 4+2, 9+4]. Notice that the numbers wrap around again. If k is negative, 
+ * the sum is of the previous numbers. 
+ * 
+ * Example 4: 
+ * Input: code = [12,29,0,24,29,4], k = 12 
+ * Output: [196,196,196,196,196,196] 
+ * Explanation: Randomly generated solution. 
+********************************************************************************************************************************/
+using System;
+
+public class Program
+{
+
+    // main 
+    public static void Main(string[] args)
+    {
+
+        // declare local variables 
+        int k;
+        Random r; 
+        int[] code, decryptedCode; 
+
+        // initialize local variables 
+        r = new Random(); 
+
+        // example 1
+        k = 3;
+        code = new int[] { 5, 7, 1, 4 }; 
+        decryptedCode = Decrypt(code, k);
+
+        // example 2 
+        k = 0;
+        code = new int[] { 1, 2, 3, 4 };
+        decryptedCode = Decrypt(code, k);
+
+        // example 3 
+        k = -2;
+        code = new int[] { 2, 4, 9, 3 };
+        decryptedCode = Decrypt(code, k);
+
+        // example 4
+        k = r.Next(-16, 17);
+        code = new int[r.Next(2, 8)]; 
+        for (int i = 0; i < code.Length; i++)
+        {
+            code[i] = r.Next(0, 33); 
+        }
+        decryptedCode = Decrypt(code, k);
+
+        // example 5
+        k = r.Next(-32, 33);
+        code = new int[r.Next(2, 16)];
+        for (int i = 0; i < code.Length; i++)
+        {
+            code[i] = r.Next(0, 65);
+        }
+        decryptedCode = Decrypt(code, k);
+
+        // example 6 
+        k = 6; 
+        code = GenerateHarmonicSequence(k, 884, null);
+        decryptedCode = Decrypt(code, k); 
+
+        // example 7 
+        k = 12;
+        code = GenerateHarmonicSequence(k, 1003, null);
+        decryptedCode = Decrypt(code, k); 
+
+        // keep the application running  
+        Console.ReadLine();
+
+    }
+
+    // decrypts the code 
+    public static int[] Decrypt(int[] code, int k)
+    {
+
+        // declare local variables 
+        int index, direction; 
+        int[] decryptedCode;
+
+        // initialize local variables 
+        index = 0; direction = -1; 
+        decryptedCode = new int[code.Length];
+
+        // only if k > 0 or k < 0 
+        if (k != 0)
+        {
+
+            // direction of the loop 
+            direction = (k > 0) ? 1 : direction;
+
+            // decrypts the code 
+            for (int i = 0; i < code.Length; i++)
+            {
+
+                // resets index 
+                index = (i + direction);
+
+                for (int j = 0; j < Math.Abs(k); j++)
+                {
+
+                    // re-posistions the index either at the
+                    // start or end of the array 
+                    // for a circular loop 
+                    index = (index == code.Length) ? 0 : index;
+                    index = (index < 0) ? code.Length - 1 : index;
+
+                    // adds the current element to the decrypted code 
+                    decryptedCode[i] += code[index];
+                    index += direction;
+
+                }
+
+            }
+
+        }
+
+        // prints the original code 
+        Console.Write("Code: "); 
+        for (int i = 0; i < code.Length; i++)
+        {
+            Console.Write(code[i] + " "); 
+        }
+        Console.WriteLine();
+
+        // prints the array 
+        Console.Write("Decrypted Code: ");
+        for (int i = 0; i < decryptedCode.Length; i++)
+        {
+            Console.Write(decryptedCode[i] + " "); 
+        }
+        Console.WriteLine();
+        Console.WriteLine("k = " + k);
+        Console.WriteLine();
+
+        // returns the decrypted code 
+        return decryptedCode; 
+
+    }
+
+    // generate a Harmonic sequence 
+    public static int[] GenerateHarmonicSequence(int size, int target, int? kFactor = null)
+    {
+
+        // declare local variables 
+        int[] harmonicCode; 
+        int factor, product, remainder;
+        int operation, index, value; 
+        Random r; 
+
+        // initialize local variables 
+        harmonicCode = new int[size];
+        factor = product = remainder = 0;
+        r = new Random(); 
+
+        // factor 
+        factor = target / size;
+
+        // product 
+        product = factor * size;
+
+        // remainder 
+        remainder = target - product; 
+
+        // fill code 
+        for (int i = 0; i < harmonicCode.Length; i++)
+        {
+            harmonicCode[i] = factor; 
+        }
+
+        // add the remainder to the parity/remainder bit 
+        harmonicCode[size - 1] += remainder;
+
+        // print array 
+        Console.WriteLine("Target: " + target);
+        Console.Write("Preliminary Code: "); 
+        PrintArray(harmonicCode);
+        Console.WriteLine(); 
+
+        // start at code[0]
+        for (int i = 0; i < harmonicCode.Length; i++)
+        {
+
+            // pick a random index 
+            index = r.Next(0, harmonicCode.Length);
+
+            // randomly pick an operation, add or subtract (0 = add, 1 = subtract) 
+            operation = r.Next(0, 2);
+
+            // random number between 0, code[secondRandomIndex] + 1)
+            value = (operation == 0) ? r.Next(0, harmonicCode[i] + 1) : r.Next(0, harmonicCode[index] + 1);
+
+            // add or subtract at code[i] 
+            harmonicCode[i] = (operation == 0) ? (harmonicCode[i] - value) : (harmonicCode[i] + value);
+
+            // add or subtract at code[index] 
+            harmonicCode[index] = (operation == 0) ? (harmonicCode[index] + value) : (harmonicCode[index] - value); 
+
+        }
+
+        // return code 
+        return harmonicCode; 
+
+    }
+
+    public static void PrintArray(int[] data)
+    {
+        for (int i = 0; i < data.Length; i++)
+        {
+            Console.Write(data[i] + " ");
+        }
+    }
+
+}
