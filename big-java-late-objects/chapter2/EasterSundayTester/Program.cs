9c9063ef52c1f705f555b518e2268ffc4a50602b
Sam Leal <Leal.Samir@Outlook.com>
2024-07-12 21:25:00 -0400

    refactor: update target framework to net8.0 in EasterSundayTester.csproj, ran prettier check on 3x Program.cs files, and added a Print method in GroupThePeople.cs solution.

--- big-java-late-objects/chapter2/EasterSundayTester/Program.cs
+++ big-java-late-objects/chapter2/EasterSundayTester/Program.cs
@@ -1,95 +1,95 @@
 ﻿/*********************************************************************************************************************
- * Name: Sam Leal 
- * Date: 08/12/2023 
- * Filename: EasterSundayTester.cs 
- * Description: Easter Sunday is the first sunday after the first full moon of spring. To compute the date, you can 
- * use this algorithm, invented by the mathematician Carl Friedrich Gauss in 1800. 
-*********************************************************************************************************************/ 
+ * Name: Sam Leal
+ * Date: 08/12/2023
+ * Filename: EasterSundayTester.cs
+ * Description: Easter Sunday is the first sunday after the first full moon of spring. To compute the date, you can
+ * use this algorithm, invented by the mathematician Carl Friedrich Gauss in 1800.
+*********************************************************************************************************************/
 public class Program
 {
 
-    // main 
+    // main
     public static void Main(string[] args)
     {
 
-        // declare local variables 
-        int year; 
+        // declare local variables
+        int year;
         Tuple<int, int> date;
 
-        // initialize local variables 
-        year = 1994; 
+        // initialize local variables
+        year = 1994;
 
-        // display the Easter Sunday dates 
+        // display the Easter Sunday dates
         while (year <= 2024)
         {
 
-            // compute the date 
+            // compute the date
             date = ComputeEasterSundayDate(year);
 
-            // display the date 
+            // display the date
             Console.WriteLine("Easter Sunday falls on " + date.Item2 + "/" + date.Item1 + "/" + year);
 
-            // increment the year 
-            year++; 
+            // increment the year
+            year++;
 
-        }   
+        }
 
-        // keep the application running 
-        Console.ReadLine(); 
+        // keep the application running
+        Console.ReadLine();
 
     }
 
-    // computes the date and returns the day, month as a Tuple 
+    // computes the date and returns the day, month as a Tuple
     public static Tuple<int, int> ComputeEasterSundayDate(int year)
     {
 
-        // declare local variables 
+        // declare local variables
         int a, b, c, d, e, g, h, j, k, m, r, n, p, y;
 
-        // initialize local variables 
+        // initialize local variables
         y = year;
 
-        // get the remainder of y/19 
+        // get the remainder of y/19
         a = y % 19;
 
         // get a quotient b
         b = y / 100;
 
-        // get a remainder c 
+        // get a remainder c
         c = y % 100;
 
         // get a quotient d
         d = b / 4;
 
-        // get a remainder e 
+        // get a remainder e
         e = b % 4;
 
-        // get a quotient g 
-        g = (8 * b + 13) / 25; // ignore the remainder 
+        // get a quotient g
+        g = (8 * b + 13) / 25; // ignore the remainder
 
-        // get a remainder h 
-        h = (19 * a + b - d - g + 15) % 30; // ignore the quotient 
+        // get a remainder h
+        h = (19 * a + b - d - g + 15) % 30; // ignore the quotient
 
-        // get a quotient j 
+        // get a quotient j
         j = c / 4;
 
-        // get a remainder k 
+        // get a remainder k
         k = c % 4;
 
-        // get a quotient m 
-        m = (a + 11 * h) / 319; // ignore the quotient 
+        // get a quotient m
+        m = (a + 11 * h) / 319; // ignore the quotient
 
-        // get a remainder r 
-        r = (2 * e + 2 * j - k - h + m + 32) % 7;  // ignore the quotient 
+        // get a remainder r
+        r = (2 * e + 2 * j - k - h + m + 32) % 7;  // ignore the quotient
 
-        // get a quotient n 
-        n = (h - m + r + 90) / 25; // ignore the remainder 
+        // get a quotient n
+        n = (h - m + r + 90) / 25; // ignore the remainder
 
         // get a remainder p
-        p = (h - m + r + n + 19) % 32; // ignore the quotient 
+        p = (h - m + r + n + 19) % 32; // ignore the quotient
 
-        // returns a new Tuple, where p = the day, and n = the month 
-        return new Tuple<int, int>(p, n); // Easter Sunday falls on year y 
+        // returns a new Tuple, where p = the day, and n = the month
+        return new Tuple<int, int>(p, n); // Easter Sunday falls on year y
 
     }
 
